daef59dcf6ede3d1303a623652110aea10903f41
Areccus Branch <88910250+areccus@users.noreply.github.com>
2023-04-01 21:17:14 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# areccus.github.io
---
+++ src/content_option.js
@@ -0,0 +1,151 @@
+const logotext = "ARECCUS";
+const meta = {
+    title: "Areccus Branch",
+    description: "I’m Areccus Branch a Full-Stack web developer",
+};
+
+const introdata = {
+    title: "I’m Areccus Branch",
+    animated: {
+        first: "I am a Full-Stack Web Developer",
+        second: "I develop cool websites",
+        third: "I develop mobile applications",
+    },
+    description: "Bringing websites to life with code! React, Javascript, CSS, HTML, SQL and Python are just a few of my tools. Let's make something magical together!",
+    your_img_url: "https://drive.google.com/uc?export=view&id=1lwMtUqiHgubReYPh3pT-h10lNLbittZY",
+};
+
+const dataabout = {
+    title: "About Myself",
+    aboutme: "Hi there! I'm Areccus, a software developer specializing in React and SQL. When I'm not coding, you'll find me exploring new technologies or playing video games. Let's connect and create something amazing!",
+};
+const worktimeline = [{
+        jobtitle: "I.T.",
+        where: "Greene County High-School",
+        date: "2018",
+    },
+    {
+        jobtitle: "FrontEnd Developer",
+        where: "Underdog Devs",
+        date: "2022",
+    },
+    {
+        jobtitle: "I.T. Support",
+        where: "America's Remanufacturing Company",
+        date: "Present",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "Python",
+        value: 75,
+    },
+    {
+        name: "SQL",
+        value: 75,
+    },
+];
+
+const services = [{
+        title: "Frontend Development",
+        description: "I specialize in front-end development using React, Javascript, HTML, and CSS. With a custom-built website designed just for you, you'll be able to reach more customers and grow your business faster.",
+    },
+    {
+        title: "Backend Development",
+        description: "I have built databases using Postgres SQL and SQLite. I'm also experienced in using Python for web scraping and basic data analyzing.",
+    },
+    {
+        title: "Mobile Application Development",
+        description: "built mobile apps, games, and more using the leading frameworks like C# and Javascript.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://drive.google.com/uc?export=view&id=1FhfdnsCBSf9ntXVJeG1G2p8GPURbOiwb",
+        desctiption: "This is the front end to Bubble. Bubble is a social media application.",
+        link: "https://bubble-client-5z5q4wgtp-areccus.vercel.app/",
+    },
+    {
+        img: "https://drive.google.com/uc?export=view&id=1DrrMwFp1YHKZLHwRQzuoBW84zp7BC5QA",
+        desctiption: "The back end to the water my plants project. This project was done with my cohorts in the Bloom Institute of Technology bootcamp.",
+        link: "https://github.com/areccus/plants-back-end",
+    },
+    {
+        img: "https://drive.google.com/uc?export=view&id=19xBK1vquVnNsJN9_td4InA9Q87YpbKrp",
+        desctiption: "UnderDog Devs is a non-profit to get individuals with low-income into tech. UnderDog Dev offers a web development program.",
+        link: "https://www.underdogdevs.org/",
+    },
+    {
+        img: "https://drive.google.com/uc?export=view&id=1h3RfzUfN3U59-SwuBL7ZJ4ky2jcKBDfq",
+        desctiption: "This is the front end to my water my plants project. You are able to get and post information about different plant species.",
+        link: "https://github.com/Water-my-plants-bw-web-49/front-end-2",
+    },
+    {
+        img: "https://drive.google.com/uc?export=view&id=1NnzFz7UhSxShxoNzfps3DXwcuuGo3tnB",
+        desctiption: "This is an anime streaming site. Watch free anime and get information from MyAnimeList.",
+        link: "https://animestreaming.vercel.app/",
+    },
+    {
+        img: "https://drive.google.com/uc?export=view&id=1U1C8ra3hJ3_GG1rMNGKPYzuY1thMUVTn",
+        desctiption: "A simple job search project using indeed.",
+        link: "https://github.com/areccus/Job_Search_App",
+    },
+
+    {
+        img: "https://drive.google.com/uc?export=view&id=1oBAMKKeApo2d17v9Dw0gTzKuhaT4t4u-",
+        desctiption: "Website made for a client Top Notch Mobile Gaming for their gaming truck. This was made using Webflow.",
+        link: "https://www.topnotchgaming.org/",
+    },
+    {
+        img: "https://drive.google.com/uc?export=view&id=1DrrMwFp1YHKZLHwRQzuoBW84zp7BC5QA",
+        desctiption: "The backend or the back bone to my social media application known as Bubble.",
+        link: "https://github.com/areccus/Bubble_Backend",
+    },
+    {
+        img: "https://drive.google.com/uc?export=view&id=1_GufBisLZ6NnPeofpKCNWeC6TMjXe3SS",
+        desctiption: "A program made for America's Remanufacturing Company using Python. Loops and prints ZPL labels with information provided.",
+        link: "https://github.com/areccus/RMA-Pallet-Sticker-Creator",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "areccusbranch92@gmail.com",
+    YOUR_FONE: "(706)816-8021",
+    description: "Please Feel free to contact me and I will get back to you as soon as possible. Thank you for your interest.",
+    // create an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_kqv2oum",
+    YOUR_TEMPLATE_ID: "template_427m73q",
+    YOUR_USER_ID: "2PbJBAMj8uNLe_GdG",
+};
+
+const socialprofils = {
+    github: "https://github.com/areccus",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/areccusbranch/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
